829ec73f605112ca3b5b03300f53a51b625f4f12
Lrac <carl.lam95@gmail.com>
2016-05-09 19:55:24 -0700

    Fixed adc component in Qsys

--- fpga/fpga_hw/top_level/DE0_Nano_SOPC_bb.v
+++ fpga/fpga_hw/top_level/DE0_Nano_SOPC_bb.v
@@ -2,11 +2,6 @@
 module DE0_Nano_SOPC (
 	UART_RXD_to_the_RS232_0,
 	UART_TXD_from_the_RS232_0,
-	adc_controller_0_conduit_end_sys_clk,
-	adc_controller_0_conduit_end_ADC_CONVST,
-	adc_controller_0_conduit_end_ADC_SDI,
-	adc_controller_0_conduit_end_ADC_SCK,
-	adc_controller_0_conduit_end_ADC_SDO,
 	clk_50,
 	reset_n,
 	in_port_to_the_key,
@@ -21,15 +16,15 @@ module DE0_Nano_SOPC (
 	kill_sw_from_the_power_management_slave_0,
 	mux_from_the_power_management_slave_0,
 	data_to_the_power_management_slave_0,
-	in_port_to_the_sw);	
+	in_port_to_the_sw,
+	adc_controller_0_conduit_end_sys_clk,
+	adc_controller_0_conduit_end_ADC_CONVST,
+	adc_controller_0_conduit_end_ADC_SCK,
+	adc_controller_0_conduit_end_ADC_SDI,
+	adc_controller_0_conduit_end_ADC_SDO);	
 
 	input		UART_RXD_to_the_RS232_0;
 	output		UART_TXD_from_the_RS232_0;
-	input		adc_controller_0_conduit_end_sys_clk;
-	output		adc_controller_0_conduit_end_ADC_CONVST;
-	output		adc_controller_0_conduit_end_ADC_SDI;
-	output		adc_controller_0_conduit_end_ADC_SCK;
-	input		adc_controller_0_conduit_end_ADC_SDO;
 	input		clk_50;
 	input		reset_n;
 	input	[1:0]	in_port_to_the_key;
@@ -45,4 +40,9 @@ module DE0_Nano_SOPC (
 	output	[2:0]	mux_from_the_power_management_slave_0;
 	input		data_to_the_power_management_slave_0;
 	input	[3:0]	in_port_to_the_sw;
+	input		adc_controller_0_conduit_end_sys_clk;
+	output		adc_controller_0_conduit_end_ADC_CONVST;
+	output		adc_controller_0_conduit_end_ADC_SCK;
+	output		adc_controller_0_conduit_end_ADC_SDI;
+	input		adc_controller_0_conduit_end_ADC_SDO;
 endmodule
